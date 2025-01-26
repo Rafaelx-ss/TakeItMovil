77a5740fdf4ae2bb0796e3e46e2c584032ec698b
Rafaelx-ss <solisrafael895@gmail.com>
2025-01-26 16:25:39 -0600

    Update endpoints.ts

--- context/endpoints.ts
+++ context/endpoints.ts
@@ -1,12 +1,11 @@
 import { Platform } from 'react-native';
 
-// backendlocal
+// backend local (TENER PRENDIDO EL BACKEND)
 // export const backend =
 //     Platform.OS === 'web'
 //         ? "http://127.0.0.1:8000"
 //         : "http://10.0.2.2:8000";
 
 
-
 // backend de produccion
-// export const backend = "https://cody.mx/sopmex/Takeitapis/public"; 
+// export const backend = "https://cody.mx/sopmex/Takeitapis/public";  
