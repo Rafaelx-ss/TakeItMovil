03c3157719a1ee3d715ef32671d3f0dc1663e556
Rafaelx-ss <solisrafael895@gmail.com>
2025-01-26 16:13:06 -0600

    Update endpoints.ts

--- context/endpoints.ts
+++ context/endpoints.ts
@@ -1,10 +1,10 @@
 import { Platform } from 'react-native';
 
 // backendlocal
-export const backend =
-    Platform.OS === 'web'
-        ? "http://127.0.0.1:8000"
-        : "http://10.0.2.2:8000";
+// export const backend =
+//     Platform.OS === 'web'
+//         ? "http://127.0.0.1:8000"
+//         : "http://10.0.2.2:8000";
 
 
 
