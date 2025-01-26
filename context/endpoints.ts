a9b03a8e62c40742cc64743b9d162ca481f7225c
Rafaelx-ss <solisrafael895@gmail.com>
2025-01-26 16:12:34 -0600

    Update endpoints.ts

--- context/endpoints.ts
+++ context/endpoints.ts
@@ -1,6 +1,12 @@
 import { Platform } from 'react-native';
 
+// backendlocal
 export const backend =
     Platform.OS === 'web'
-        ? "http://127.0.0.1:8000" // URL para web
-        : "http://10.0.2.2:8000"; // URL para móvil
+        ? "http://127.0.0.1:8000"
+        : "http://10.0.2.2:8000";
+
+
+
+// backend de produccion
+// export const backend = "https://cody.mx/sopmex/Takeitapis/public"; 
